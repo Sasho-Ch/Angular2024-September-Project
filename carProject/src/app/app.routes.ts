25d255db72b6846ba0bc705eeac143f85c08dbd5
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-12-15 22:53:03 +0300

    adding user components

--- carProject/src/app/app.routes.ts
+++ carProject/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
 
 export const routes: Routes = [
     {path: '',redirectTo:'/home' , pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'profile', component: ProfileComponent},
 ];
